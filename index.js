1c21fefa9c69cb91259f185605a7952aed11fc79
hiroshi noguchi <hiroshi.noguchi@leonisand.co>
2017-12-06 12:25:17 +0900

    Support Node 4.3

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
+"use strict";
+
 const Vis = require('./graphvis');
-let Sequelize;
-const relationship = ({source, target, associationType, as}) => {
-  const typeString = {
-    BelongsTo: `[arrowtail=crow, arrowhead=odot, dir=both]`,
-    BelongsToMany: `[arrowtail=crow, arrowhead=crow, dir=both, label="${as}"]`,
-  }[associationType];
-  if (typeString) {
-    return `"${source.name}" -> "${target.name}" ${typeString}`;
-  }
-}
+const ModelTemplate = require('./lib/templates/model');
+const AssociationTemplate = require('./lib/templates/association');
 
-const typeName = (columnType) => {
-  for(let name in Sequelize.DataTypes) {
-    let type = Sequelize.DataTypes[name];
-    if(columnType instanceof type && name !== 'ABSTRACT') {
-      return name;
-    }
-  }
-}
-
-const attributeTemplate = attribute => `${attribute.fieldName} :${typeName(attribute.type)}\\l\\`;
-
-const modelTemplate = model => `"${model.name}" [shape=record, label="{${model.name}|\
-      ${Object.values(model.attributes).map(attributeTemplate).join('\n')}
-    }"]`;
-
-module.exports = path => {
+module.exports = (path) => {
   const db = typeof path === 'string' ? require(path) : path;
-  Sequelize = db.constructor;
+  const Sequelize = db.constructor;
   const models = Object.values(db.models);
+  const modelTemplate = new ModelTemplate(Sequelize);
+  const associationTemplate = new AssociationTemplate();
   return Vis(`
     digraph models_diagram {
       graph[overlap=false, splines=true]
-      ${models.map(modelTemplate).join('\n')}
-      ${models.map(model => Object.values(model.associations).map(relationship).join('\n')).join('\n')}
+      ${modelTemplate.render(models)}
+      ${associationTemplate.render(models)}
   }`, {
     format: 'svg',
     engine: 'dot'
